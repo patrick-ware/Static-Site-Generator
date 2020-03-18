345b743a357ff9eb97b2cafc1133259e0195a3d6
Patrick Ware <patrick.r.ware@gmail.com>
2020-03-17 21:38:23 -0700

    bash compiler output directory changed to docs, resume.html added to compiler

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-cat templates/top.html content/index.html templates/bottom.html > combined_index.html
-cat templates/top.html content/aboutme.html templates/bottom.html > combined_aboutme.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/aboutme.html templates/bottom.html > docs/aboutme.html
+cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
